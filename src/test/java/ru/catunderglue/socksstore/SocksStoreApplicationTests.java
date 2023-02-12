96bbcc707e02ae5107af260a7336e9ae0017721e
CatUnderGlue <warmonds1996@gmail.com>
2023-02-12 11:10:40 +0500

    Initial commit

---
+++ src/test/java/ru/catunderglue/socksstore/SocksStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.catunderglue.socksstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
